ad5030ba21950ec6a5f1a6a52fa28d55e3818e5e
daruzymatos <daruzy.matos@aluno.senai.br>
2025-04-04 10:57:51 -0300

    Add files via upload

---
+++ Cadastro.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastro;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Cadastro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String studentName = " Daruzy ";
+        int studentID = 15;
+        int studentAge = 23;
+        float studentFee = 75.25f;
+        char studentGrade = 'B';
+        
+        // Print variables
+        System.out.println("Student Nome: "+ studentName);
+        System.out.println("Student ID: "+ studentID);
+        System.out.println("Student Age: "+ studentAge);
+        System.out.println("Student Fee: "+ studentFee);
+        System.out.println("Student Grade: "+ studentGrade);
+    }
+    
+}
